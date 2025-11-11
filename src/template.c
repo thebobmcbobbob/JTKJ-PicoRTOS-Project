ab0a022952df00e5895aa011533f2949267b458d
Arda Önal <ardaonal968@gmail.com>
2025-11-11 16:20:31 +0200

    arda

--- src/template.c
+++ src/template.c
@@ -9,13 +9,203 @@
 #include <task.h>
 
 #include "tkjhat/sdk.h"
+#include "tusb.h"//the library used to create a serial port over USB, according to part 5 
 
 // Default stack size for the tasks. It can be reduced to 1024 if task is not using lot of memory.
 #define DEFAULT_STACK_SIZE 2048 
 
 //Add here necessary states
-enum state { IDLE=1 };
-enum state programState = IDLE;
+enum state {WAITING=1, WRITE_TO_MEMORY, SEND_MESSAGE, UPPER_IDLE, UPPER_PROCESSING}; //// IIVARI changed || upper_idle = 0 || to || upper_idle ||
+enum state upper_state = UPPER_IDLE;
+enum state lower_state = WAITING;
+char received_morse_code[256] = {0};//buffer to store 
+bool message_received = false;
+
+
+
+
+
+char current_morse;
+
+char morse_message[257];
+
+int morse_index = 0;
+
+static void read_sensor(void *arg) {
+    printf("read_sensor started %d\n", lower_state);
+    (void) arg;
+    float ax, ay, az, gx, gy, gz, t;
+
+    if (lower_state != WAITING)
+        return;  // Do nothing if not ready to detect motion
+
+    init_ICM42670();
+
+    ICM42670_start_with_default_values();
+
+    if (ICM42670_read_sensor_data(&ax, &ay, &az, &gx, &gy, &gz, &t) == 0)
+    {
+        if (az > 0.1) {
+            printf("UP: %.2fg)\n", az); // delete after testing
+            current_morse = '.';
+        }
+        else if (az < -0.1) {
+            printf("DOWN: %.2fg)\n", az); // delete after testing
+            current_morse = '-';
+        }
+        printf("lower state changed\n");
+        lower_state = WRITE_TO_MEMORY;
+    }
+    
+    vTaskDelay(pdMS_TO_TICKS(100)); 
+}
+
+
+static void read_button() {
+    printf("read_button started %d\n", lower_state);
+    if (lower_state == WRITE_TO_MEMORY) {
+        while (1) {
+            printf("passed state check \n");
+            if (current_morse != '\0' && morse_index < 257) 
+            {
+                morse_message[morse_index++] = current_morse;
+                morse_message[morse_index] = '\0'; /// keep string terminated
+                printf("Stored: %c | Buffer: %s\n", current_morse, morse_message); /// only for testing
+            }
+
+            current_morse = '\0';
+            lower_state = WAITING;  /// Ready for next motion
+
+        }
+    }
+
+}
+
+
+
+
+void tud_cdc_rx_cb(uint8_t itf){
+
+    // allocate buffer for the data on the stack    
+    uint8_t buf[CFG_TUD_CDC_RX_BUFSIZE + 1];
+
+    uint32_t count = tud_cdc_n_read(itf, buf, sizeof(buf));// reads data from USB into buf. You’ll then process that data as needed. 
+
+    if (itf == 1) {//add the data to received_morse_code
+        for (int i = 0; i < count && i < sizeof(received_morse_code)-1; i++) {
+            received_morse_code[i] = buf[i];}
+        
+        if (count < sizeof(received_morse_code)) {//terminate c string
+            received_morse_code[count] = '\0';} 
+
+        message_received = true;
+ 
+        tud_cdc_n_write(itf, (uint8_t const *) "OK\n", 3); //be gentle and send an ok back
+        tud_cdc_n_write_flush(itf);
+    }
+
+    // Optional: if you need a C-string, you can terminate it:
+    // if (count < sizeof(buf)) buf[count] = '\0';
+}
+
+
+static void usbTask(void *arg) {
+    (void)arg;  
+    while (1) {
+        tud_task();              // With FreeRTOS, wait for events
+                                 // Do not add vTaskDelay.
+    }
+}
+
+
+void morse_code_light(char* morse_code){//turn received morse into led light intervals
+
+    for (int i=0; morse_code[i] !='\n' && morse_code[i] !='\0';i++){
+        //message ends with two spaces and new line(\n) according to the doc so it should recognize it?
+        if (morse_code[i] == ' ' && morse_code[i+1] == ' ' && morse_code[i+2] == '\n') {break;}
+
+
+        if (morse_code[i] == '.') {
+            set_led_status(true);
+            vTaskDelay(pdMS_TO_TICKS(100));//amount of ticks to indicate its a dot
+            set_led_status(false);
+            vTaskDelay(pdMS_TO_TICKS(100));
+        } 
+
+        else if (morse_code[i] == '-') { 
+            set_led_status(true);
+            vTaskDelay(pdMS_TO_TICKS(100));//amount of ticks to indicate its a dash
+            set_led_status(false);
+            vTaskDelay(pdMS_TO_TICKS(100)); 
+        }
+
+        else if (morse_code[i] == ' ') {
+            if (morse_code[i+1] == ' ') {
+                vTaskDelay(pdMS_TO_TICKS(100));}//amount of ticks to indicate space between two words
+             
+        
+            else {vTaskDelay(pdMS_TO_TICKS(100));} //amount of ticks to indicate its a space
+        }              
+                
+                
+        //NOTES; kind of confused on how to differentiate spaces and the inherent timing between dots and dashes
+    }}
+
+
+static void display_task(void *arg) {
+    (void)arg;
+    
+    init_display();
+    init_led();
+    set_led_status(false);
+
+    clear_display();
+    write_text("ready");
+
+    for(;;){    
+        switch (upper_state){
+
+            case UPPER_IDLE:
+                if (message_received){
+                    upper_state = UPPER_PROCESSING;}
+                break;
+
+            
+            case (UPPER_PROCESSING):
+                clear_display();
+                write_text("Message from workstation:");
+                vTaskDelay(pdMS_TO_TICKS(3000)); 
+                clear_display();
+                write_text(received_morse_code);//see if it works after testing
+                vTaskDelay(pdMS_TO_TICKS(5000)); 
+                morse_code_light(received_morse_code);//sse if it works after testing
+                vTaskDelay(pdMS_TO_TICKS(10000));  
+
+                clear_display();
+                write_text("waiting for message");
+                message_received=false;
+                upper_state = UPPER_IDLE;
+                break;
+            
+
+            case SEND_MESSAGE:// state machine to trigger send_message has to be done, i havent considered it yet this is just the code.
+                clear_display();
+                write_text("Message from sensor:");
+                vTaskDelay(pdMS_TO_TICKS(5000));  
+                clear_display();
+                write_text(morse_message);
+                vTaskDelay(pdMS_TO_TICKS(10000));
+                clear_display();
+                morse_index = 0;
+                morse_message[0] = '\0';
+                upper_state = UPPER_IDLE;
+                break;  }
+
+
+    vTaskDelay(pdMS_TO_TICKS(1000));//change later 
+    }
+}
+
 
 static void example_task(void *arg){
     (void)arg;
@@ -29,30 +219,66 @@ static void example_task(void *arg){
 int main() {
     stdio_init_all();
     // Uncomment this lines if you want to wait till the serial monitor is connected
-    /*while (!stdio_usb_connected()){
+    while (!stdio_usb_connected()){
         sleep_ms(10);
-    }*/ 
+    } 
+    
+    printf("first print worked");
     init_hat_sdk();
     sleep_ms(300); //Wait some time so initialization of USB and hat is done.
+    printf("init successful");
+    TaskHandle_t sensorTask, buttonTask, displayTask, hUsb = NULL;
+    
 
-    TaskHandle_t myExampleTask = NULL;
     // Create the tasks with xTaskCreate
-    BaseType_t result = xTaskCreate(example_task,       // (en) Task function
-                "example",              // (en) Name of the task 
-                DEFAULT_STACK_SIZE, // (en) Size of the stack for this task (in words). Generally 1024 or 2048
-                NULL,               // (en) Arguments of the task 
-                2,                  // (en) Priority of this task
-                &myExampleTask);    // (en) A handle to control the execution of this task
+    //BaseType_t result = xTaskCreate(example_task,       // (en) Task function
+    //            "example",              // (en) Name of the task 
+    //            DEFAULT_STACK_SIZE, // (en) Size of the stack for this task (in words). Generally 1024 or 2048
+    //            NULL,               // (en) Arguments of the task 
+    //            2,                  // (en) Priority of this task
+    //            &myExampleTask);    // (en) A handle to control the execution of this task
 
-    if(result != pdPASS) {
-        printf("Example Task creation failed\n");
+    // Create the sensor task
+    BaseType_t result = xTaskCreate(read_sensor, "read_sensor", DEFAULT_STACK_SIZE, NULL, 2, &sensorTask);
+    if (result != pdPASS) {
+        printf("Sensor Task creation failed\n");
+        return 0;
+    }
+    printf("readsensor");
+    // Create the button task
+    result = xTaskCreate(read_button, "read_button", DEFAULT_STACK_SIZE, NULL, 2, &buttonTask);
+    if (result != pdPASS) {
+        printf("Button Task creation failed\n");
+        return 0;
+    }
+    printf("readbutton");
+
+
+
+    // Create the display task
+    result = xTaskCreate(display_task, "display_task", DEFAULT_STACK_SIZE, NULL, 2, &displayTask);             
+    
+    if (result != pdPASS) {
+        printf("Display Task creation failed\n");
         return 0;
     }
 
-    // Start the scheduler (never returns)
+    // Create the usb task
+    result = xTaskCreate(usbTask, "usb_task", DEFAULT_STACK_SIZE, NULL, 3, &hUsb);//priority 3
+
+    if (result != pdPASS) {
+        printf("usb task creation failed\n");
+        return 0;
+    }
+
+
+    // Start the scheduler
+    tusb_init();
     vTaskStartScheduler();
 
-    // Never reach this line.
+    while (1){
+        printf("failed :(");
+    }
+    // Never reach this line
     return 0;
 }
-
